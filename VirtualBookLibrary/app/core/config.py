017f1667e64ef1c19c5a2c9d94408442e124b079
Jay M <60815668+jaymoundekar18@users.noreply.github.com>
2026-02-13 16:27:59 +0530

    Add files via upload

---
+++ VirtualBookLibrary/app/core/config.py
@@ -0,0 +1,3 @@
+API_BASE_URL = "https://virtualbooklibrary-tuf0.onrender.com" 
+APP_TITLE = "Virtual Book Library"
+CURRENT_USER_ID = None
---
+++ VirtualBookLibrary/app/main.py
@@ -0,0 +1,75 @@
+import streamlit as st
+from app.core.config import APP_TITLE
+from app.screens import home, dashboard, authentication, account
+
+# Page Configurations
+st.set_page_config(page_title=APP_TITLE, layout="wide")
+st.title(APP_TITLE)
+
+
+# SESSION STATE INITIALIZATION
+# Authentication state
+if "authenticated" not in st.session_state:
+    st.session_state.authenticated = False
+
+# Current page
+if "page" not in st.session_state:
+    st.session_state.page = "None"
+
+# Sidebar visibility
+if "sidebar_visible" not in st.session_state:
+    st.session_state.sidebar_visible = False
+
+
+# AUTHENTICATION ROUTING
+# If user is NOT authenticated
+if not st.session_state.authenticated:
+    st.session_state.sidebar_visible = False
+    authentication.render()
+    st.stop() 
+
+
+#IF AUTHENTICATED
+st.session_state.sidebar_visible = True
+
+# If user just logged in and page not set → default to Home
+if st.session_state.page == "None":
+    st.session_state.page = "Home"
+
+
+# SIDEBAR NAVIGATION
+st.sidebar.info(
+    "Use the refresh button to update the dashboard.\n"
+    "⚠️ Do Not refresh using CTRL+R session will be lost!"
+)
+
+st.sidebar.title("Navigation")
+
+if st.sidebar.button("Refresh", icon="🔄", key="refresh_button"):
+    st.rerun()
+
+if st.sidebar.button("Home", icon="🏠", key="home_button",width=200):
+    st.session_state.page = "Home"
+
+if st.sidebar.button("Dashboard", icon="📊", key="dashboard_button",width=200):
+    st.session_state.page = "Dashboard"
+
+if st.sidebar.button("Account", icon="👤", key="account_button",width=200):
+    st.session_state.page = "Account"
+
+if st.sidebar.button("Logout", icon="↩️", key="logout_button",width=200):
+    st.session_state.authenticated = False
+    st.session_state.page = "None"
+    st.session_state.logged_user = None
+    st.session_state.current_user_id = None
+    st.rerun()
+
+# PAGE RENDERING
+if st.session_state.page == "Home":
+    home.render()
+
+elif st.session_state.page == "Dashboard":
+    dashboard.show_dashboard()
+
+elif st.session_state.page == "Account":
+    account.render()
