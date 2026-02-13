624d3ed93e88ba6e980d267f9e637a47d06f4c7f
Jay M <60815668+jaymoundekar18@users.noreply.github.com>
2026-02-13 16:30:21 +0530

    Update home.py

--- VirtualBookLibrary/app/screens/home.py
+++ VirtualBookLibrary/app/screens/home.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from app.services.api_client import APIClient
-from app.utils.helpers import is_valid_email
+from services.api_client import APIClient
+from utils.helpers import is_valid_email
 
 def render():
     st.session_state.user_details = APIClient.get_user(st.session_state.current_user_id)
